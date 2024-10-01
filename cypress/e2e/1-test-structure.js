2254b1236477d7841007f3078239a037ed1a22d2
ihsan <ozgeihsan@gma>
2024-10-01 14:27:13 +0300

    cypressProject intial settings and files added

---
+++ cypress/e2e/1-test-structure.js
@@ -0,0 +1,37 @@
+describe('First test suite',()=>{
+
+    describe('suite section',()=>{
+        beforeEach('login',()=>{
+            //repeat for every test
+        })
+        it('first test', ()=>{
+            // put the test code
+        })
+        it('second test', ()=>{
+            // put the test code
+        })
+    })
+
+    it('first test', ()=>{
+        // put the test code
+    })
+    it('second test', ()=>{
+        // put the test code
+    })
+    it('third test', ()=>{
+        // put the test code
+    })
+})
+
+describe('Second test suite',()=>{
+
+    it('first test', ()=>{
+        // put the test code
+    })
+    it('second test', ()=>{
+        // put the test code
+    })
+    it('third test', ()=>{
+        // put the test code
+    })
+})
